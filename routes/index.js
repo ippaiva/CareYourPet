3d0c4a1d966555fc37857663d2da1140c6849772
Ronaldo Rodrigues da Silva <neolondres@outlook.com>
2018-11-19 20:32:39 -0200

    adicionado rotas para pet hotel

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../models/user")
+const User = require("../models/user");
+const Pet = require("../models/pet");
+const Hotel = require("../models/hotel");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -33,10 +35,46 @@ router.get('/user', (req, res, next) => {
 });
 
 router.post('/user', (req, res, next) => {
-  const { CPF, name, lastName, street.streetAddress, city, state, cep, phone } = req.body;
+  const { CPF, name, lastName, adress:streetAddress, address:city, address:state, address:cep, phone } = req.body;
   console.log(req.body);
-  const newUser = new User({ CPF, name, lastName, streetAddress, city, state, cep, phone })
+  const newUser = new User({ CPF, name, lastName, adress:streetAddress, address:city, address:state, address:cep, phone });
   newUser.save()
+  .then(() => {
+    res.redirect('/home');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+// Pet form GET and POST
+router.get('/pet', (req, res, next) => {
+  res.render('forms/pet');
+});
+
+router.post('/pet', (req, res, next) => {
+  const { name, type, race, size, age, genero, cor } = req.body;
+  console.log(req.body);
+  const newPet = new Pet({ name, type, race, size, age, genero, cor });
+  newPet.save()
+  .then(() => {
+    res.redirect('/home');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+// Hotel form GET and POST
+router.get('/hotel', (req, res, next) => {
+  res.render('forms/hotel');
+});
+
+router.post('/hotel', (req, res, next) => {
+  const { name, CNPJ, address:streetAddress, address:city, address:state, address:cep } = req.body;
+  console.log(req.body);
+  const newHotel = new Hotel({ name, CNPJ, address:streetAddress, address:city, address:state, address:cep });
+  newHotel.save()
   .then(() => {
     res.redirect('/home');
   })
